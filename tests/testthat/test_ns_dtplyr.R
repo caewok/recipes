cd2b9995e72ebbc3a251ce031c8d86d8e614637e
Michael Enion <caewok_2000@yahoo.com>
2020-12-14 17:41:59 +0000

    Add test_ns_dtplyr. All tests passed.

---
+++ tests/testthat/test_ns_dtplyr.R
@@ -0,0 +1,82 @@
+library(testthat)
+library(recipes)
+library(modeldata)
+data(biomass)
+library(splines)
+library(dtplyr)
+
+context("Natural splines")
+
+biomass_tr <- biomass[biomass$dataset == "Training",]
+biomass_te <- biomass[biomass$dataset == "Testing",]
+
+biomass_tr_dt <- lazy_dt(biomass_tr)
+biomass_te_dt <- lazy_dt(biomass_te)
+iris_dt <- lazy_dt(iris)
+
+rec <- recipe(HHV ~ carbon + hydrogen + oxygen + nitrogen + sulfur,
+              data = biomass_tr_dt)
+
+test_that('correct basis functions', {
+  with_ns <- rec %>%
+    step_ns(carbon, hydrogen)
+
+  with_ns <- prep(with_ns, training = biomass_tr_dt, verbose = FALSE)
+
+  with_ns_pred_tr <- bake(with_ns, new_data = biomass_tr_dt, composition = "tibble")
+  with_ns_pred_te <- bake(with_ns, new_data = biomass_te_dt, composition = "tibble")
+
+  # expected, using the original
+  carbon_ns_tr_exp <- ns(biomass_tr$carbon, df = 2)
+  hydrogen_ns_tr_exp <- ns(biomass_tr$hydrogen, df = 2)
+  carbon_ns_te_exp <- predict(carbon_ns_tr_exp, biomass_te$carbon)
+  hydrogen_ns_te_exp <- predict(hydrogen_ns_tr_exp, biomass_te$hydrogen)
+
+  carbon_ns_tr_res <- as.matrix(with_ns_pred_tr[, grep("carbon", names(with_ns_pred_tr))])
+  colnames(carbon_ns_tr_res) <- NULL
+  hydrogen_ns_tr_res <- as.matrix(with_ns_pred_tr[, grep("hydrogen", names(with_ns_pred_tr))])
+  colnames(hydrogen_ns_tr_res) <- NULL
+
+  carbon_ns_te_res <- as.matrix(with_ns_pred_te[, grep("carbon", names(with_ns_pred_te))])
+  colnames(carbon_ns_te_res) <- 1:ncol(carbon_ns_te_res)
+  hydrogen_ns_te_res <- as.matrix(with_ns_pred_te[, grep("hydrogen", names(with_ns_pred_te))])
+  colnames(hydrogen_ns_te_res) <- 1:ncol(hydrogen_ns_te_res)
+
+  ## remove attributes
+  carbon_ns_tr_exp <- matrix(carbon_ns_tr_exp, ncol = 2)
+  carbon_ns_te_exp <- matrix(carbon_ns_te_exp, ncol = 2)
+  hydrogen_ns_tr_exp <- matrix(hydrogen_ns_tr_exp, ncol = 2)
+  hydrogen_ns_te_exp <- matrix(hydrogen_ns_te_exp, ncol = 2)
+  dimnames(carbon_ns_tr_res) <- NULL
+  dimnames(carbon_ns_te_res) <- NULL
+  dimnames(hydrogen_ns_tr_res) <- NULL
+  dimnames(hydrogen_ns_te_res) <- NULL
+
+  expect_equal(carbon_ns_tr_res, carbon_ns_tr_exp)
+  expect_equal(carbon_ns_te_res, carbon_ns_te_exp)
+  expect_equal(hydrogen_ns_tr_res, hydrogen_ns_tr_exp)
+  expect_equal(hydrogen_ns_te_res, hydrogen_ns_te_exp)
+})
+
+
+test_that('printing', {
+  with_ns <- rec %>%  step_ns(carbon, hydrogen)
+  expect_output(print(with_ns))
+  expect_output(prep(with_ns, training = biomass_tr_dt, verbose = TRUE))
+})
+
+
+test_that('tunable', {
+  rec <-
+    recipe(~ ., data = iris_dt) %>%
+    step_ns(all_predictors())
+  rec_param <- tunable.step_ns(rec$steps[[1]])
+  expect_equal(rec_param$name, c("deg_free"))
+  expect_true(all(rec_param$source == "recipe"))
+  expect_true(is.list(rec_param$call_info))
+  expect_equal(nrow(rec_param), 1)
+  expect_equal(
+    names(rec_param),
+    c('name', 'call_info', 'source', 'component', 'component_id')
+  )
+})
